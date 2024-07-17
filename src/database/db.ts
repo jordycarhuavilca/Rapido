a62bce478809f573eba3e9acbe5e52b850b66bab
jordy <jordycarhuavilca2021@gmail.com>
2024-07-17 09:57:53 -0500

    fix

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,22 +1,22 @@
 import mongoose from 'mongoose';
 
 const db = {
-    connect : async () => {
-        try {
-            const mongodbUri = `${process.env.MONGO_SRV}${process.env.DATABASE}`;
-            mongoose.connection.on('error', err=>{
-                console.log(`error ${err}`)
-            })
-            mongoose.connection.on('disconnected', err=>{
-                console.log(`error disconnected ${err}`)
-            })
-            return await mongoose.connect(mongodbUri,{
-                serverSelectionTimeoutMS : 2000,
-                socketTimeoutMS: 2500
-            });
-        } catch (error) {
-            console.log(`trycatch error ${error}`)
-        }
+  connect: async () => {
+    try {
+      const mongodbUri = `${process.env.MONGO_SRV}${process.env.DATABASE}`;
+      mongoose.connection.on('error', (err) => {
+        console.log(`error ${err}`);
+      });
+      mongoose.connection.on('disconnected', (err) => {
+        console.log(`disconnected error ${err}`);
+      });
+      return await mongoose.connect(mongodbUri, {
+        serverSelectionTimeoutMS: 2000,
+        socketTimeoutMS: 2500
+      });
+    } catch (error) {
+      console.log(`try catch error ${error}`);
     }
-}
+  }
+};
 export default db;
