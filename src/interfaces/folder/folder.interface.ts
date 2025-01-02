2ab60901b602fbbc683a451e8616e082267bca3c
jordy <jordycarhuavilca2021@gmail.com>
2025-01-02 18:59:26 -0500

    fixing

--- src/interfaces/folder/folder.interface.ts
+++ src/interfaces/folder/folder.interface.ts
@@ -1,6 +1,7 @@
 import { file } from '@interfaces/file/file.interface';
 import { v4 as uuidv4 } from 'uuid';
 import { typeObject } from '@utils/object'; 
+import { Folder } from '@entities/Folder.entity';
 export interface addFolderDto {
   id: uuidv4;
   name: string;
@@ -8,7 +9,7 @@ export interface addFolderDto {
   numFiles: number;
   location: string;
   type: typeObject;
-  objects?: Array<folder | file >;
+  objects?: Array<Folder | file >;
   userId: string;
   creationDate?: Date;
   updatedDate?: Date;
